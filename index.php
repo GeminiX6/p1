579d922016cedf8cf66076c38c9844c957a1071f
Matt Cole <coledawg@gmail.com>
2015-09-15 22:45:50 -0400

    Added page and picture.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Matthew Cole P1 - DWA15 Portfolio</title>
+	<meta charset='utf-8'>
+
+  <link rel="stylesheet" type="text/css" <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">>
+
+  <style>
+
+    h1
+    {
+      text-align: center;
+    }
+
+    img
+    {
+      display: block;
+      margin: auto;
+      width: 40%;
+    }
+
+    .wrapper
+    {
+      width: 75%;
+      min-width: 768px;
+      margin-top: 50px;
+      margin-bottom: 50px;
+    }
+
+  </style>
+
+</head>
+<body>
+
+  <div class="wrapper">
+
+    <h1>Matthew Cole <br> DWA15 Portfolio </h1>
+
+    <img src= "matt.jpg" alt="Matt Cole"
+
+  <div class="pure-menu pure-menu-horizontal">
+      <a href="#" class="pure-menu-heading pure-menu-link">PROJECTS</a>
+      <ul class="pure-menu-list">
+          <li class="pure-menu-item pure-menu-disabled"><a href="#" class="pure-menu-link">P2</a></li>
+          <li class="pure-menu-item pure-menu-disabled"><a href="#" class="pure-menu-link">P3</a></li>
+          <li class="pure-menu-item pure-menu-disabled"><a href="#" class="pure-menu-link">P4</a></li>
+      </ul>
+  </div>
+
+  <h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>My name is Matthew Cole, and I am a computer technician at General Dynamics Information Technology. I fix computers, assist with various software installations and updates, and maintain servers and network equipment. I also do a lot of work within my company regarding virtual machines.</p>
+
+		<h2>Why are you taking this course?</h2>
+		<p>I'm working towards my masters in Information Management Systems so that I can grow within my company.</p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have programmed in HTML, CSS, C++ and Java.</p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working on a PC that I built myself from scratch, running Windows 10.</p>
+
+		<h2>Anything else we should know?</h2>
+		<p>I love Megaman!</p>
+
+</div>
+
+</body>
+</html>
